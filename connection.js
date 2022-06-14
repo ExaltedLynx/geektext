066bca7cae9e845435d73ef945b7ad83875365dd
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2022-06-14 17:46:14 -0400

    Saved DB credentials

--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 
 const pool= new Pool
 	({
-		host: "localhost",
-		user: "geektext",
+		host: "ec2-52-44-13-158.compute-1.amazonaws.com",
+		user: "pqmykrpeklbmmw",
 		port: 5432,
-		password: "geektext",
-		database: "geektext"
+		password: "084747d347c86b5c8989c67e8049210a5d176bbf15a0f3454147a698afdd1ddd",
+		database: "dcol1553kdbata"
 	})
 
 module.exports = pool;
