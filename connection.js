3f237574439b9f5966b0e0bf7b0ab80b18d28a75
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2022-06-14 18:07:12 -0400

    Fixed credential connection information so conn Pool works

--- connection.js
+++ connection.js
@@ -1,13 +1,15 @@
 const { Pool } = require('pg');
 
-const pool= new Pool
-	({
-		host: "ec2-52-44-13-158.compute-1.amazonaws.com",
-		user: "pqmykrpeklbmmw",
-		port: 5432,
-		password: "084747d347c86b5c8989c67e8049210a5d176bbf15a0f3454147a698afdd1ddd",
-		database: "dcol1553kdbata"
-	})
+const pool= new Pool ({
+	host: "ec2-52-44-13-158.compute-1.amazonaws.com",
+	user: "pqmykrpeklbmmw",
+	port: 5432,
+	password: "084747d347c86b5c8989c67e8049210a5d176bbf15a0f3454147a698afdd1ddd",
+	database: "dcol1553kdbata",
+  	ssl: {    /* <----- Add SSL option */
+    rejectUnauthorized: false,
+  }
+})
 
 module.exports = pool;
 
