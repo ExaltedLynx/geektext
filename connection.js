70ee770dfa30b3cd3de92283d45e4c2700d12d15
JuanSebastianC9 <juansebas_99@hotmail.com>
2022-07-30 23:19:32 -0400

    update user info

--- connection.js
+++ connection.js
@@ -68,13 +68,115 @@ const postUpdateUser = (request, response) => {
 		var query = 'SELECT username, password FROM objects.users WHERE username = $1 AND password = $2';
 		query = query.replace('%20', ' ');
 
-		var values = [username, password]
+		var values = [username, password];
 
 		pool.query(query, values, (error, results) => {
-			response.status(200).json(results);
+			//response.status(200).json(results);
 			if (error) {
-				console.log(error)
+				console.log(error);
 			};
+		
+			console.log(results.rows);
+
+			if(results.rows.length > 0 && request.body.newpassword != null){
+				var queryUpdate = 'UPDATE objects.users SET password = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newpassword].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew password is: "+request.body.newpassword);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newname != null){
+				var queryUpdate = 'UPDATE objects.users SET name = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newname].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew name is: "+request.body.newname);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newaddress1 != null){
+				var queryUpdate = 'UPDATE objects.users SET address_1 = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newaddress1].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew Address_1 is: "+request.body.newaddress1);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newaddress2 != null){
+				var queryUpdate = 'UPDATE objects.users SET address_2 = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newaddress2].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew Address_2 is: "+request.body.newaddress2);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newcity != null){
+				var queryUpdate = 'UPDATE objects.users SET city = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newcity].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew City is: "+request.body.newcity);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newstate != null){
+				var queryUpdate = 'UPDATE objects.users SET state = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newstate].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew State is: "+request.body.newstate);
+					console.log(resultsupdate);
+				})
+			};
+			if(results.rows.length > 0 && request.body.newzip != null){
+				var queryUpdate = 'UPDATE objects.users SET zip = $1 WHERE username = $2 AND password = $3';
+				var valuesUpdate = [request.body.newzip].concat(values);
+				console.log(queryUpdate, valuesUpdate);
+
+				pool.query(queryUpdate, valuesUpdate, (errorupdate, resultsupdate) => {
+					console.log(resultsupdate);
+					if (errorupdate) {
+						console.log(errorupdate);
+					};
+					console.log("Username: "+username+"\nNew Zip is: "+request.body.newzip);
+					console.log(resultsupdate);
+				})
+			};
+			response.status(200).json(results.rows);
 		})
 	};
 	
